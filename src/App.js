173f9cd729ab2c85880b3a284229f6ab8f299b33
techguyash <techguyashash@gmail.com>
2022-11-19 14:33:10 +0530

    todo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,23 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 function App() {
+  const INITIAL_STATE = [
+    {
+      id: "1",
+      value: "this is one",
+    },
+    {
+      id: "2",
+      value: "this is two",
+    },
+    {
+      id: "3",
+      value: "this is three",
+    },
+  ];
+
+  const [todo, setTodo] = useState(INITIAL_STATE);
+
   return (
     <div className="App">
       <Header />
