4672b6b5b6e53f14a8d7b2c9e81aeb4aedde7e01
techguyash <techguyashash@gmail.com>
2022-11-19 11:49:18 +0530

    two

--- src/Header.js
+++ src/Header.js
@@ -3,20 +3,34 @@ import "./Header.css";
 
 const Header = () => {
   const [value, setValue] = useState("");
+  const [error, setError] = useState(false);
 
   const inputHandler = (e) => {
     setValue(e.target.value);
   };
 
+  const submitHandler = (e) => {
+    e.preventDefault(); //
+    if (value.length > 3) {
+      console.log("Ok");
+      setError(false);
+    } else {
+      console.log("not ok");
+      setError(true);
+    }
+  };
+
   return (
     <div className="container">
-      <form className="form">
+      <form className="form" onSubmit={submitHandler}>
         <input
           type="text"
           className="todo"
           value={value}
           onChange={inputHandler}
         />
+        {error && <p style={{ color: "white" }}>Invalid Text</p>}
+
         <button type="submit">Create</button>
       </form>
     </div>
